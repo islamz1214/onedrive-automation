34e6ac20923501cfe08b61dd48299377e3a7f89f
ZahidulIslam <zahidulislam@pop-os.localdomain>
2020-08-20 21:08:43 -0400

    Correctly add empty downloads folder to repo, switch config.js to .env for best practice local testing, and update environment variables for both local and CI

--- tests/onedrive.js
+++ tests/onedrive.js
@@ -1,7 +1,5 @@
 'use strict'
-const { config } = require('../config.js')
 const { exec } = require("child_process");
-
 
 
 module.exports = {
@@ -11,10 +9,10 @@ module.exports = {
     signInPage
       .navigate()
       .waitForElementVisible('@emailInput')
-      .setValue('@emailInput', config.email || ONEDRIVE_EMAIL)
+      .setValue('@emailInput', process.env.EMAIL || $ONEDRIVE_EMAIL)
       .click('@nextSignInButton')
       .waitForElementVisible('@passwordInput')
-      .setValue('@passwordInput', config.password || ONEDRIVE_PASSWORD)
+      .setValue('@passwordInput', process.env.PASSWORD || $ONEDRIVE_PASSWORD)
       .waitForElementVisible('@nextSignInButton')
       .click('@nextSignInButton')
   },
